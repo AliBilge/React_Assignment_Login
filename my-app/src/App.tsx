0982069f96d8fb3c1da2ec0d9664e940a3de85a7
Ali <alibilge@yahoo.ca>
2020-04-11 21:54:30 -0600

    Heading added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Heading} from './components/Heading';
 import logo from './logo.svg';
 import {Button, Icon} from 'semantic-ui-react';
 import './App.css';
@@ -7,8 +8,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Heading />
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 >This is my awsome header</h1>
+        <h2>This is My Awsome START</h2>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
