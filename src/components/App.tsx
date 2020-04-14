b59735fc2f9477e5d6e72c93938d0e96f4047a40
Ali <alibilge@yahoo.ca>
2020-04-14 02:45:24 -0600

    Modified Version

---
+++ src/components/App.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import '../styles/App.css';
+import { Fragment } from 'react';
+import MainSection from './MainSection';
+import SectionWithLoopRendering from './SectionWithLoopRendering';
+import ValidationForm from './Validation';
+
+export interface IAppProps {
+}
+
+interface IState {
+  isButtonPressed: boolean;
+  userName: string;
+}
+
+export default class App extends React.Component<IAppProps, IState> {
+  constructor(props: IAppProps) {
+    super(props);
+
+    this.state = {
+      isButtonPressed: false,
+      userName: "default"
+    }
+  }
+
+  public onClickButton = () => {
+    this.setState({ isButtonPressed: true })
+  }
+
+  public onClickButtonToGoBack = () => {
+    this.setState({ isButtonPressed: false })
+  }
+
+  public onClickUserNameSetButton = (newUserName: string) => {
+    console.log(this.state)
+    this.setState({ userName: newUserName });
+  }
+
+  public render() {
+    let { isButtonPressed, userName } = this.state;
+    return (
+      <Fragment>
+        <div className="App"> 
+          <MainSection
+            isButtonPressed={isButtonPressed}
+            callbackMethod={this.onClickButton}
+            callbackMethodToGoBack={this.onClickButtonToGoBack}
+            userName={userName}
+            onClickUserNameSetButton={this.onClickUserNameSetButton} />
+            <ValidationForm />
+            <div>
+            {isButtonPressed ?
+                        <SectionWithLoopRendering />
+                        :
+                        <h1>Please Login</h1>
+                    }
+            </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
